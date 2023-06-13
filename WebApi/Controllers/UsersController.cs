5a44f14fc1d07c87aa2d598433d6eb528384904e
Marvelous-iAcademy <130442240+Marvelous-iAcademy@users.noreply.github.com>
2023-06-13 13:46:25 +0100

    Add project files.

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController 
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {     
+            return _context.Users.Find(id);
+        }
+
+    }
+}
